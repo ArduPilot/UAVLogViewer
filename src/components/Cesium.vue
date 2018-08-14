1cc49f092dbebe515b0d9c428dfa43b064509aaf
Willian Galvani <williangalvani@gmail.com>
2018-08-13 22:07:44 -0300

    Cesium: Enable fps overlay

--- src/components/Cesium.vue
+++ src/components/Cesium.vue
@@ -38,6 +38,7 @@ export default {
           requestRenderMode: true,
           shouldAnimate: false
         })
+      this.viewer.scene.debugShowFramesPerSecond = true
     }
     this.plotTrajectory(this.trajectory)
   },
