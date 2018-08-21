a85485411667e56dbea28d5fa01e5cdb27dcc755
Willian Galvani <williangalvani@gmail.com>
2018-08-21 15:25:14 -0300

    add flask backend redirection on dev environment

--- config/index.js
+++ config/index.js
@@ -10,7 +10,16 @@ module.exports = {
     // Paths
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
-    proxyTable: {},
+    proxyTable: {
+      "/upload/*":{
+        target:"http://localhost:8001/upload",
+        secure:"false"
+      },
+      "/uploaded/*":{
+        target:"http://localhost:8001/uploaded",
+        secure:"false"
+      },
+    },
 
     // Various Dev Server settings
     host: 'localhost', // can be overwritten by process.env.HOST
