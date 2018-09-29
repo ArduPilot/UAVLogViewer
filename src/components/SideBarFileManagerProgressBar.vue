a85b797270e555b93a3e9dc4cec964d7aec6bfb9
Willian Galvani <williangalvani@gmail.com>
2018-09-29 15:39:25 -0300

    ProgressBar: Hide after completed

--- src/components/SideBarFileManagerProgressBar.vue
+++ src/components/SideBarFileManagerProgressBar.vue
@@ -1,5 +1,5 @@
-<template>
-  <div class="progress1">
+<template >
+  <div class="progress1" v-bind:id="[percent > 99 ? 'hideMe' : '']">
     <b-progress  height="30px" :max="100">
       <b-progress-bar :value="percent">
         <strong>{{ percent < 100 ? percent.toFixed(1): complete }}</strong>
@@ -29,4 +29,27 @@ export default {
   transition: none;
 }
 
+#hideMe {
+  -moz-animation: cssAnimation 5s ease-in 5s forwards;
+  /* Firefox */
+  -webkit-animation: cssAnimation 5s ease-in 5s forwards;
+  /* Safari and Chrome */
+  -o-animation: cssAnimation 5s ease-in 5s forwards;
+  /* Opera */
+  animation: cssAnimation 5s ease-in 5s forwards;
+  -webkit-animation-fill-mode: forwards;
+  animation-fill-mode: forwards;
+}
+@keyframes cssAnimation {
+  to {
+    opacity: 0;
+    overflow:hidden;
+  }
+}
+@-webkit-keyframes cssAnimation {
+  to {
+    opacity: 0;
+    height: 0;
+  }
+}
 </style>
