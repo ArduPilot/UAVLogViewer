707cec58d58bb3bea262e19c9898da49fab9478b
Willian Galvani <williangalvani@gmail.com>
2019-03-30 20:11:21 -0300

    SideBarMessageMenu: omit units when they are not present

--- src/components/SideBarMessageMenu.vue
+++ src/components/SideBarMessageMenu.vue
@@ -23,7 +23,7 @@
                     <template v-for="item in message.complexFields">
                         <li class="field" v-bind:key="key+'.'+item.name" @click="toggle(key, item.name)">
                             <a v-if="isPlottable(key,item.name)"> {{item.name}}
-                                ({{item.units}})</a>
+                                <span v-if="item.units!=='?'"> ({{item.units}})</span></a>
                         </li>
                     </template>
                 </b-collapse>
