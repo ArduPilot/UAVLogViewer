df01b091bc9cfd27364c24aec8339ac30487227f
biakp <biababykp@gmail.com>
2019-11-06 19:55:50 -0300

    Removed useless CSS code and made some design changes

--- src/components/PlotSetup.vue
+++ src/components/PlotSetup.vue
@@ -14,7 +14,7 @@
                     <select v-model.number="field.axis">
                         <option v-bind:key="'axisnumber'+axis" v-for="axis in state.allAxis">{{axis}}</option>
                     </select>
-                    <select :style="{color:  field.color}" v-model="field.color">
+                    <select :style="{color: field.color}" v-model="field.color">
                         <option
                             v-bind:key="'axisColor'+color"
                             :style="{color: color}"
@@ -48,133 +48,7 @@ export default {
 </script>
 <style>
 
-    .nav-side-menu ul,
-    .nav-side-menu li {
-        list-style: none;
-        padding: 0;
-        margin: 0;
-        line-height: 35px;
-        cursor: pointer;
-        /*
-          .collapsed{
-             .arrow:before{
-                       font-family: FontAwesome;
-                       content: "\f053";
-                       display: inline-block;
-                       padding-left:10px;
-                       padding-right: 10px;
-                       vertical-align: middle;
-                       float:right;
-                  }
-           }
-      */
-    }
-
-    .nav-side-menu ul .sub-menu li.active a,
-    .nav-side-menu li .sub-menu li.active a {
-        color: #d19b3d;
-    }
-
-    .nav-side-menu ul .sub-menu li,
-    .nav-side-menu li .sub-menu li {
-        background-color: #181c20;
-        border: none;
-        line-height: 28px;
-        border-bottom: 1px solid #23282e;
-        margin-left: 0;
-    }
-
-    .nav-side-menu ul .sub-menu li:hover,
-    .nav-side-menu li .sub-menu li:hover {
-        background-color: #020203;
-    }
-
-    .nav-side-menu ul .sub-menu li:before,
-    .nav-side-menu li .sub-menu li:before {
-        font-family: FontAwesome;
-        content: "\f105";
-        display: inline-block;
-        padding-left: 10px;
-        padding-right: 10px;
-        vertical-align: middle;
-    }
-
-    .nav-side-menu li {
-        padding-left: 0;
-        border-left: 3px solid #2e353d;
-        border-bottom: 1px solid #23282e;
-    }
-
-    .nav-side-menu li a {
-        text-decoration: none;
-        color: #e1ffff;
-    }
-
-    .nav-side-menu li a i {
-        padding-left: 10px;
-        width: 20px;
-        padding-right: 20px;
-    }
-
-    .nav-side-menu li:hover {
-        border-left: 3px solid #d19b3d;
-        background-color: #4f5b69;
-        -webkit-transition: all 1s ease;
-        -moz-transition: all 1s ease;
-        -o-transition: all 1s ease;
-        transition: all 1s ease;
-    }
-
-    @media (max-width: 767px) {
-
-        main {
-            height: 90%;
-            margin-top: 50px;
-        }
-    }
-
-    @media (min-width: 767px) {
-
-        main {
-            height: 100%;
-        }
-    }
-
-    i {
-        margin: 10px;
-    }
-
-    ::-webkit-scrollbar {
-        width: 12px;
-        background-color: rgba(0, 0, 0, 0);
-    }
-
-    ::-webkit-scrollbar-thumb {
-        border-radius: 5px;
-        box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.1);
-        -webkit-box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.1);
-        -moz-box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.1);
-        background-color: #1c437f;
-    }
-
-    .tabholder a {
-        border-top: 1px solid white;
-        border-left: 1px solid white;
-        border-right: 1px solid white;
-        border-radius: 3px;
-        padding-left: 5px;
-        padding-right: 5px;
-        font-weight: bold;
-        background-color: #2E353D;
-    }
-
-    .tabholder a:hover {
-        background-color: #4f5b69;
-        -webkit-transition: all 1s ease;
-        -moz-transition: all 1s ease;
-        -o-transition: all 1s ease;
-        transition: all 1s ease;
-    }
+    /* COLOR PICKER */
 
     ul.colorpicker {
         font-family: 'FontAwesome';
@@ -207,6 +81,23 @@ export default {
         overflow: hidden;
         direction: rtl;
         display: block;
+    }
+
+/* MEDIA QUERIES */
+
+@media (max-width: 767px) {
+
+        main {
+            height: 90%;
+            margin-top: 50px;
+        }
+    }
+
+    @media (min-width: 767px) {
+
+        main {
+            height: 100%;
+        }
     }
 
 </style>
