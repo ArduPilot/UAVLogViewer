a3a98c01d938e371ceb36270933847be04632f47
Willian Galvani <williangalvani@gmail.com>
2019-11-26 19:27:49 -0300

    use python to serve build files

--- test/e2e/runner.js
+++ test/e2e/runner.js
@@ -2,48 +2,41 @@
 // 1. start the dev server using production config
 process.env.NODE_ENV = 'testing'
 
-const webpack = require('webpack')
-const DevServer = require('webpack-dev-server')
-
-const webpackConfig = require('../../build/webpack.prod.conf')
 const devConfigPromise = require('../../build/webpack.dev.conf')
-
-let server
+var exec = require('child_process').exec
 
 devConfigPromise.then(devConfig => {
-  const devServerOptions = devConfig.devServer
-  const compiler = webpack(webpackConfig)
-  server = new DevServer(compiler, devServerOptions)
-  const port = devServerOptions.port
-  const host = devServerOptions.host
-  return server.listen(port, host)
+    exec('cd $(pwd)/dist && python3 -m http.server 8080', function callback(error, stdout, stderr){
+        console.log(error)
+        console.log(stdout)
+        console.log(stderr)
+    });
 })
-.then(() => {
-  // 2. run the nightwatch test suite against it
-  // to run in additional browsers:
-  //    1. add an entry in test/e2e/nightwatch.conf.js under "test_settings"
-  //    2. add it to the --env flag below
-  // or override the environment flag, for example: `npm run e2e -- --env chrome,firefox`
-  // For more information on Nightwatch's config file, see
-  // http://nightwatchjs.org/guide#settings-file
-  let opts = process.argv.slice(2)
-  if (opts.indexOf('--config') === -1) {
-    opts = opts.concat(['--config', 'test/e2e/nightwatch.conf.js'])
-  }
-  if (opts.indexOf('--env') === -1) {
-    opts = opts.concat(['--env', 'chrome'])
-  }
+    .then(() => {
+        // 2. run the nightwatch test suite against it
+        // to run in additional browsers:
+        //    1. add an entry in test/e2e/nightwatch.conf.js under "test_settings"
+        //    2. add it to the --env flag below
+        // or override the environment flag, for example: `npm run e2e -- --env chrome,firefox`
+        // For more information on Nightwatch's config file, see
+        // http://nightwatchjs.org/guide#settings-file
+        let opts = process.argv.slice(2)
+        if (opts.indexOf('--config') === -1) {
+            opts = opts.concat(['--config', 'test/e2e/nightwatch.conf.js'])
+        }
+        if (opts.indexOf('--env') === -1) {
+            opts = opts.concat(['--env', 'chromeHeadless'])
+        }
 
-  const spawn = require('cross-spawn')
-  const runner = spawn('./node_modules/.bin/nightwatch', opts, { stdio: 'inherit' })
+        const spawn = require('cross-spawn')
+        const runner = spawn('./node_modules/.bin/nightwatch', opts, { stdio: 'inherit' })
+        runner.on('exit', function (code) {
+            exec('pkill -9 -f http.server')
+            process.exit(code)
+        })
 
-  runner.on('exit', function (code) {
-    server.close()
-    process.exit(code)
-  })
-
-  runner.on('error', function (err) {
-    server.close()
-    throw err
-  })
-})
+        runner.on('error', function (err) {
+            exec('pkill -9 -f http.server')
+            throw err
+        })
+    })
