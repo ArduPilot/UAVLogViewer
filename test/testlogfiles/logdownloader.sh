1d6ddfc851fd3db742266a6b42eac116ed892c36
Willian Galvani <williangalvani@gmail.com>
2019-12-03 19:48:21 -0300

    Add cache for test files and npm, disable unit tests in Windows

--- test/testlogfiles/logdownloader.sh
+++ test/testlogfiles/logdownloader.sh
@@ -1,15 +1,21 @@
 #!/usr/bin/env bash
-wget http://autotest.ardupilot.org/HeliCopter-test.tlog
-wget http://autotest.ardupilot.org/ArduPlane-test.tlog
-wget http://autotest.ardupilot.org/APMrover2-test.tlog
-wget http://autotest.ardupilot.org/ArduSub-test.tlog
-wget http://autotest.ardupilot.org/QuadPlane-test.tlog
-wget http://autotest.ardupilot.org/BalanceBot-test.tlog
-wget http://autotest.ardupilot.org/AntennaTracker-test.tlog
-wget http://autotest.ardupilot.org/ArduCopter-test.tlog
-truncate --size=1M *.tlog
-wget http://autotest.ardupilot.org/ArduSub-log.bin
-wget http://autotest.ardupilot.org/ArduCopter-log.bin
-wget http://autotest.ardupilot.org/APMrover2-log.bin
-wget http://autotest.ardupilot.org/ArduPlane-log.bin
-truncate --size=1M *.bin
+if [ -d "/tmp/testlogs" ]
+then
+    echo "cache exists, quitting"
+else
+    mkdir /tmp/testlogs
+    wget http://autotest.ardupilot.org/HeliCopter-test.tlog --directory-prefix=/tmp/testlogs/
+    wget http://autotest.ardupilot.org/ArduPlane-test.tlog --directory-prefix=/tmp/testlogs/
+    wget http://autotest.ardupilot.org/APMrover2-test.tlog --directory-prefix=/tmp/testlogs/
+    wget http://autotest.ardupilot.org/ArduSub-test.tlog --directory-prefix=/tmp/testlogs/
+    wget http://autotest.ardupilot.org/QuadPlane-test.tlog --directory-prefix=/tmp/testlogs/
+    wget http://autotest.ardupilot.org/BalanceBot-test.tlog --directory-prefix=/tmp/testlogs/
+    wget http://autotest.ardupilot.org/AntennaTracker-test.tlog --directory-prefix=/tmp/testlogs/
+    wget http://autotest.ardupilot.org/ArduCopter-test.tlog --directory-prefix=/tmp/testlogs/
+    truncate --size=10M /tmp/testlogs/*.tlog
+    wget http://autotest.ardupilot.org/ArduSub-log.bin --directory-prefix=/tmp/testlogs/
+    wget http://autotest.ardupilot.org/ArduCopter-log.bin --directory-prefix=/tmp/testlogs/
+    wget http://autotest.ardupilot.org/APMrover2-log.bin --directory-prefix=/tmp/testlogs/
+    wget http://autotest.ardupilot.org/ArduPlane-log.bin --directory-prefix=/tmp/testlogs/
+    truncate --size=10M /tmp/testlogs/*.bin
+fi
--- test/unit/specs/dataflashlogs.spec.js
+++ test/unit/specs/dataflashlogs.spec.js
@@ -2,7 +2,7 @@ let dfparser = require('../../../src/tools/parsers/dataflashParser.js')
 var glob = require('glob')
 
 // options is optional
-let files = glob.sync(require('path').join(__dirname, '../../testlogfiles/*.bin'))
+let files = glob.sync('/tmp/testlogs/*.bin')
 console.log(files)
 describe('parse binary logs', () => {
     test.each(files)('parse %s', (a) => {
