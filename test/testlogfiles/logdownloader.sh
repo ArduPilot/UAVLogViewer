a7da36f6c78fc2201c8701a839152cb3109b097e
Willian Galvani <williangalvani@gmail.com>
2019-12-03 00:09:40 -0300

    add test for dataflash logs

--- test/testlogfiles/logdownloader.sh
+++ test/testlogfiles/logdownloader.sh
@@ -8,3 +8,8 @@ wget http://autotest.ardupilot.org/BalanceBot-test.tlog
 wget http://autotest.ardupilot.org/AntennaTracker-test.tlog
 wget http://autotest.ardupilot.org/ArduCopter-test.tlog
 truncate --size=1M *.tlog
+wget http://autotest.ardupilot.org/ArduSub-log.bin
+wget http://autotest.ardupilot.org/ArduCopter-log.bin
+wget http://autotest.ardupilot.org/APMrover2-log.bin
+wget http://autotest.ardupilot.org/ArduPlane-log.bin
+truncate --size=1M *.bin
---
+++ test/unit/specs/dataflashlogs.spec.js
@@ -0,0 +1,18 @@
+let dfparser = require('../../../src/tools/parsers/dataflashParser.js')
+var glob = require('glob')
+
+// options is optional
+let files = glob.sync(require('path').join(__dirname, '../../testlogfiles/*.bin'))
+console.log(files)
+describe('parse binary logs', () => {
+    test.each(files)('parse %s', (a) => {
+        const fs = require('fs')
+        const logfile = fs.readFileSync(a)
+        const parser = new dfparser.DataflashParser()
+        // Hide these two
+        self.postMessage = function (a) {
+        }
+        parser.processData(logfile.buffer)
+        expect(Object.keys(parser.messageTypes)).toContain('MODE')
+    })
+})
