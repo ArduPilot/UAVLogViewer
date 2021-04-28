74938efe3b3211fbdd7b69ba3ad6701b5ddb5c89
Willian Galvani <williangalvani@gmail.com>
2021-04-28 16:43:45 -0300

    Ci: Use Soaring tlog instead of Arduplane tlog for tests

--- test/testlogfiles/logdownloader.sh
+++ test/testlogfiles/logdownloader.sh
@@ -3,7 +3,7 @@ if [ ! -d "/tmp/testlogs" ]; then
     echo "log files not found, downloading..."
     mkdir /tmp/testlogs
     wget http://autotest.ardupilot.org/HeliCopter-test.tlog --directory-prefix=/tmp/testlogs/
-    wget http://autotest.ardupilot.org/ArduPlane-test.tlog --directory-prefix=/tmp/testlogs/
+    wget http://autotest.ardupilot.org/Soaring-test.tlog --directory-prefix=/tmp/testlogs/
     wget http://autotest.ardupilot.org/APMrover2-test.tlog --directory-prefix=/tmp/testlogs/
     wget http://autotest.ardupilot.org/ArduSub-test.tlog --directory-prefix=/tmp/testlogs/
     wget http://autotest.ardupilot.org/QuadPlane-test.tlog --directory-prefix=/tmp/testlogs/
