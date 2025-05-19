63f179cb9fc034ae92d8c37d88120e68ad1189af
Willian Galvani <williangalvani@gmail.com>
2025-05-19 20:22:57 -0300

    Allow passing Cesium Ion token via environment variables

--- config/dev.env.js
+++ config/dev.env.js
@@ -3,5 +3,6 @@ const { merge } = require('webpack-merge')
 const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
-  NODE_ENV: '"development"'
+  NODE_ENV: '"development"',
+  VUE_APP_CESIUM_TOKEN: JSON.stringify(process.env.VUE_APP_CESIUM_TOKEN || '')
 })
