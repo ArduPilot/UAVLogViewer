f2b6df46a3fb91f112546e53b2d49f6bd82e0a8a
Willian Galvani <williangalvani@gmail.com>
2026-02-25 16:30:37 -0300

    mymavextra: add wrap_180 and wrap_360

--- src/mavextra/mymavextra.js
+++ src/mavextra/mymavextra.js
@@ -20,5 +20,29 @@ window.angle_diff = (angle1, angle2) => {
     return ret
 }
 
+// eslint-disable-next-line camelcase
+window.wrap_360 = (angle) => {
+    let ret = angle
+    if (ret > 360) {
+        ret -= 360
+    }
+    if (ret < 0) {
+        ret += 360
+    }
+    return ret
+}
+
+// eslint-disable-next-line camelcase
+window.wrap_180 = (angle) => {
+    let ret = angle
+    if (ret > 180) {
+        ret -= 360
+    }
+    if (ret < -180) {
+        ret += 360
+    }
+    return ret
+}
+
 window.min = Math.min
 window.max = Math.max
